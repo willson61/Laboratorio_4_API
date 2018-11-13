0e79310ab05ab371f8c0e26adaa5ef575a1a818f
Davidov249 <davidov249@gmail.com>
2018-11-13 14:31:38 -0600

    Metodos actualizar y eliminar

--- API_Pizzeria/routes/Pizzas.js
+++ API_Pizzeria/routes/Pizzas.js
@@ -5,43 +5,61 @@ const mongoClient = require('mongodb').MongoClient
 const url = 'mongodb://localhost:27017'
 const dbName = 'Pizzeria'
 
-/* GET listing, all pizzas*/
-router.get('/', function(req, res, next) {
-  mongoClient.connect(url, {useNewUrlParser: true}, (err, client) =>{
-      if(err) return next(createError(500))
-      const database = client.db(dbName)
-      const collection = database.collection('Pizzas')
-      collection.find({}).toArray((err,docs) => {
-          if(err) return next(createError(500))
-          res.status(200).json(docs);
-      })
-  })
-  //res.render('index', { title: 'Express' });
-});
+//var busqueda = 'pepperoni'
 
 /* GET specific pizza */
-/* router.get('/', (req, res, next) => {
+router.get('/', (req, res, next) => {
     mongoClient.connect(url, {useNewUrlParser: true}, (err, client) =>{
         if(err) return next(createError(500))
         const database = client.db(dbName)
         const collection = database.collection('Pizzas')
-        collection.find({})
+        collection.find({"nombre" : req.query.nombre}).toArray((err,docs) => {
+            if(err) return next(createError(500))
+            res.status(200).json(docs);
+        })
     })
 
-});*/
+});
 
+/* PUT update pizza*/
+router.put('/', (req, res, next) =>{
+    mongoClient.connect(url, {useNewUrlParser: true}, (err, client)=> {
+        if (err) return next(createError(500))
+        const database = client.db(dbName)
+        const collection = database.collection('Pizzas')
+        collection.findOneAndUpdate({"nombre": req.query.nombre}, {$set : req.body},function(err, client) {
+            if (err) throw err;
+            //console.log("1 document updated");
+            res.status(200).end();
+    })
+})
+})
 /* POST pizza */
 router.post('/', (req, res, next) => {
-    mongoClientl.connect(url, {useNewUrlParser : true}, (err, client) =>{
+    mongoClient.connect(url, {useNewUrlParser : true}, (err, client) =>{
         if(err) return next(createError(500))
         const database = client.db(dbName)
         const collection = database.collection('Pizzas')
         collection.insertOne(req.body, err =>{
             if(err) return next(createError(500))
-            res.status(201).end();
+            res.status(201).end()
+        })
+    });
+
+});
+
+/*DELETE pizza*/
+router.delete('/', (req, res, next) => {
+    mongoClient.connect(url, {useNewUrlParser: true}, (err, client) =>{
+        if(err) return next(createError(500))
+        const database = client.db(dbName)
+        const collection = database.collection('Pizzas')
+        collection.deleteOne({"nombre" : req.query.nombre}, function(err, client) {
+            if (err) throw err;
+            //console.log("1 document deleted")
+            res.status(200).end()
         })
     })
-
 });
 
 module.exports = router;
