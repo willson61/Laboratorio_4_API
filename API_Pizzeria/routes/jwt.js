b0cf47279ed843c083a920673e5eb8cfe4df9ca5
willson61 <31348545+willson61@users.noreply.github.com>
2018-11-14 22:48:57 -0600

    JSONWEBTOKEN implementacion de la verificacion

--- API_Pizzeria/routes/jwt.js
+++ API_Pizzeria/routes/jwt.js
@@ -9,10 +9,34 @@ router.post('/:jwt', function(req, res){
     res.status(200).json(token)
 })
 
+router.get('/verify/:jwt', ensureToken, function(req, res){
+    var secret1 = req.params.jwt
+    jwt.verify(req.token, secret1, function(err, data){
+        if(err){
+            res.sendStatus(403)
+        }
+        else{
+            res.status(200).json(data)
+        }
+    })
+})
+
+function ensureToken(req, res, next) {
+    const bearerHeader = req.headers["authorization"];
+    if (typeof bearerHeader !== 'undefined') {
+      const bearer = bearerHeader.split(" ");
+      const bearerToken = bearer[1];
+      req.token = bearerToken;
+      next();
+    } else {
+      res.sendStatus(403);
+    }
+  }
+
 function generarToken(json, clave){
     jwtsimple.encode(json, clave)
     return token = jwt.sign(json, clave, {
-        expiresIn: 60
+        expiresIn: 30
     })
 }
 
